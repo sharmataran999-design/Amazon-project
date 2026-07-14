90af3c162411997ca708d4281fe2555286be2eaa
sharmataran999-design <sharmataran999@gmail.com>
2026-07-14 07:13:57 +0530

    added modules

--- backend/amazon.js
+++ backend/amazon.js
@@ -1,31 +1,5 @@
-const products = [{
-  image:'images/products/athletic-cotton-socks-6-pairs.jpg',
-  name:' Black and Gray Athletic Cotton Socks - 6 Pairs',
-  rating:{
-    stars:4.5,
-    count:87
-  },
-  priceCents:1090
-},
-{
-   image:'images/products/intermediate-composite-basketball.jpg',
-  name:'Intermediate Size Basketball',
-  rating:{
-    stars:4,
-    count:127
-  },
-  priceCents:2095 
-},
-{
- image:'images/products/adults-plain-cotton-tshirt-2-pack-teal.jpg',
-  name:'Adults Plain Cotton T-Shirt - 2 Pack',
-  rating:{
-    stars:4.5,
-    count:56
-  },
-  priceCents:799
-}];
-
+import {cart} from '../data/cart.js';
+import {product} from '../data/products.js'
 let productsHTML=``;
 
 products.forEach((product) => {
@@ -72,13 +46,61 @@ products.forEach((product) => {
             Added
           </div>
 
-          <button class="add-to-cart-button button-primary">
+          <button class="add-to-cart-button button-primary js-add-to-cart"
+          data-product-id="${product.id}">
             Add to Cart
           </button>
         </div>
         `;
 });
 
-console.log(productsHTML);
-
 document.querySelector('.jsproducts-grid').innerHTML=productsHTML;
+
+const addedMessageTimeouts = {};
+
+document.querySelectorAll('.js-add-to-cart')
+  .forEach((button) => {
+    button.addEventListener('click', () => {
+
+      const productId = button.dataset.productId;
+      let matchingItem;
+
+      cart.forEach((item) => {
+
+        if (productId === item.productId) {
+          matchingItem = item;
+        }
+      });
+
+      if (matchingItem) {
+        matchingItem.quantity += 1;
+      } else {
+        cart.push({
+          productId: productId, 
+          quantity: 1
+        });
+      }
+
+      let cartQuantity=0;
+      cart.forEach((item)=>{
+        cartQuantity += item.quantity;
+      });
+      document.querySelector('.js-cart-quantity').innerHTML = cartQuantity;
+
+      const productContainer = button.closest('.product-container');
+      const messageElement = productContainer.querySelector('.added-to-cart');
+
+      messageElement.classList.add('added-to-cart-visible');
+
+      const previousTimeoutId = addedMessageTimeouts[productId];
+      if (previousTimeoutId) {
+        clearTimeout(previousTimeoutId);
+      }
+
+      const timeoutId = setTimeout(() => {
+        messageElement.classList.remove('added-to-cart-visible');
+      }, 2000);
+
+      addedMessageTimeouts[productId] = timeoutId;
+    });
+  });
